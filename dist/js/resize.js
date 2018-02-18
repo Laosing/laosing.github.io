9c8e5f33d8cb9828999f000ae123df9bba7d5ef4
Andrew <andrew.rattana@gmail.com>
2018-02-18 11:17:56 -0800

    fix mobile

--- dist/js/resize.js
+++ dist/js/resize.js
@@ -1,1 +1,1 @@
-"use strict";var windowResize=function(){$(window).resize(function(i){var e=$(i.currentTarget).height(),n=e>450?e:450;$("#work-container, .main, .work, .side, .about, .bg").css("height",n)}),$(window).resize()}();
+"use strict";var windowResize=($(window).resize(function(i){var e=$(i.currentTarget).height(),r=e>450?e:450;$("#work-container, .main, .work, .side, .about, .bg").css("height",r)}),void $(window).resize());
